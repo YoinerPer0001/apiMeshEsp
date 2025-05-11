498c809707535d7ebd94528db5f7ec954a6cad3f
yoiner <yoinerpertuz@gmail.com>
2025-05-11 13:26:44 -0500

    app moved

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import db from './src/core/db.js'
-import lecturasRoutes from './src/routes/lecturaRoutes.js'
+import db from './core/db.js'
+import lecturasRoutes from './routes/lecturaRoutes.js'
 import cors from 'cors'
 
 const app = express()
@@ -13,6 +13,6 @@ const initialPath = "/api"
 app.use(initialPath, lecturasRoutes)
 
 
-app.listen('0.0.0.0', ()=>{
+app.listen(3000,'0.0.0.0', ()=>{
     console.log("app success port: 3000" )
 })
